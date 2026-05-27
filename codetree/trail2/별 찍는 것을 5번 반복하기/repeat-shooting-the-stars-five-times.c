9bc1f4d03cb6021d4c63a5220aeb21348c4a7ee9
qkrrkdtj <149243446+qkrrkdtj@users.noreply.github.com>
2026-05-27 16:46:28 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (1ms, 0MB)

---
+++ codetree/trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_star(void) {
+    printf("**********\n");
+}
+
+int main() {
+    // Please write your code here.
+    print_star();
+    print_star();
+    print_star();
+    print_star();
+    print_star();
+    return 0;
+}
