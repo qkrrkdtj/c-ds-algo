50f99708636bef636de85d417768bb166439bff7
qkrrkdtj <149243446+qkrrkdtj@users.noreply.github.com>
2026-06-05 23:29:25 +0900

    [Passed] 흰검 칠하기 (26ms, 23MB)

--- codetree/trail2/흰검 칠하기/painting-white-black.c
+++ codetree/trail2/흰검 칠하기/painting-white-black.c
@@ -2,53 +2,47 @@
 #include <string.h>
 
 int main() {
+    // Please write your code here.
     int white[2000001];
     int black[2000001];
-    int color[2000001]; // 0: 없음, 1: 흰색, 2: 검은색
-
-    memset(white, 0, sizeof(white));
-    memset(black, 0, sizeof(black));
-    memset(color, 0, sizeof(color));
+    int color[2000001];
+    memset(white, 0, 2000001*sizeof(int));
+    memset(black, 0, 2000001*sizeof(int));
+    memset(color, 0, 2000001*sizeof(int));
 
     int N;
     scanf("%d", &N);
-
-    int pos = 1000000;
+    int x;
+    char dir;
+    int zp = 1000000;
 
     for(int i = 0; i < N; i++) {
-        int x;
-        char dir;
-
         scanf("%d %c", &x, &dir);
-
-        if(dir == 'R') {
-            for(int j = pos; j < pos + x; j++) {
+        if(dir=='R') {
+            for(int j = zp; j < zp + x; j++) {
                 black[j]++;
                 color[j] = 2;
             }
-            pos += x - 1;
+            zp += x - 1;
         }
         else {
-            for(int j = pos; j > pos - x; j--) {
+            for(int j = zp; j > zp - x; j--) {
                 white[j]++;
                 color[j] = 1;
             }
-            pos -= x - 1;
+            zp -= x - 1;
         }
     }
-
-    int w = 0, b = 0, g = 0;
-
-    for(int i = 0; i <= 2000000; i++) {
-        if(white[i] >= 2 && black[i] >= 2)
-            g++;
-        else if(color[i] == 1)
-            w++;
-        else if(color[i] == 2)
-            b++;
+    int w = 0;
+    int b = 0;
+    int g = 0;
+    for(int k = 0; k < 2000001; k++) {
+        if(white[k]>=2 && black[k]>=2) g++;
+        else if(color[k] == 1) w++;
+        else if(color[k] == 2) b++;
     }
 
-    printf("%d %d %d\n", w, b, g);
-
+    printf("%d %d %d", w, b, g);
+    
     return 0;
 }
